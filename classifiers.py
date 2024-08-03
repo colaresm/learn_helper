7e186cef863a93165c7721e8bcbdfa6d88cd207b
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2024-08-03 14:50:50 -0300

    Insert classifiers functions

---
+++ classifiers.py
@@ -0,0 +1,4 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+
--- feature_extration.py
+++ feature_extration.py
@@ -32,7 +32,7 @@ def fine_tune_model(X_train, X_test, y_train, y_test, num_classes,base_model):
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(datagen.flow(X_train, y_train, batch_size=32),validation_data=(X_test, y_test),epochs=2)
+    model.fit(datagen.flow(X_train, y_train, batch_size=32),validation_data=(X_test, y_test),epochs=5)
 
     feature_extractor = Model(inputs=model.input, outputs=base_model.output)
     X_train_features = feature_extractor.predict(X_train)
@@ -45,4 +45,4 @@ def fine_tune_model(X_train, X_test, y_train, y_test, num_classes,base_model):
     X=np.concatenate((X_train_features_flat, X_test_features_flat), axis=1)
     y=np.concatenate((y_train, y_test), axis=1)
     
-    return X, y
+    return X_train_features_flat, X_test_features_flat, y_train, y_test
